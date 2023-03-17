2378304b1f7716c0469b2414dbd665b167e20a9a
Minh Tô <milkissily@gmail.com>
2023-03-17 19:45:21 +0700

    first commit

---
+++ calculator.js
@@ -0,0 +1,14 @@
+module.exports = {
+  add: function (a, b) {
+    return a + b;
+  },
+  subtract: function (a, b) {
+    return a - b;
+  },
+  multiply: function (a, b) {
+    return a * b;
+  },
+  division: function (a, b) {
+    return a / b;
+  }
+};
---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const calculator = require('./calculator');
+const app = express();
+const port = 3000;
+const expressHbs = require('express-handlebars');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.engine("hbs", expressHbs.engine({ extname: "hbs", defaultLayout: false, layoutsDir: "views/layouts/", }));
+
+app.set('view engine', '.hbs');
+app.set('views', './views');
+
+app.get('/', function (req, res) {
+  const optionValues = ["add", "subtract", "multiply", "divide"];
+  const selectedOptionObject = optionValues.reduce(
+    (acc, option) => ({
+      ...acc,
+      [option]: option === "add",
+    }),
+    {}
+  );
+  res.render('index', {
+    selectedOption: selectedOptionObject,
+  });
+});
+
+
+app.post('/', (req, res) => {
+  const num1 = parseFloat(req.body.num1)
+  const num2 = parseFloat(req.body.num2)
+  const selectedOption = req.body.operation
+
+  let result
+
+  switch (selectedOption) {
+    case "add":
+      result = calculator.add(num1, num2);
+      break
+    case "subtract":
+      result = calculator.subtract(num1, num2);
+      break
+    case "multiply":
+      result = calculator.multiply(num1, num2);
+      break
+    case "divide":
+      result = calculator.division(num1, num2);
+      break
+  }
+
+  const optionValues = ["add", "subtract", "multiply", "divide"];
+  const selectedOptionObject = optionValues.reduce(
+    (acc, option) => ({
+      ...acc,
+      [option]: option === selectedOption,
+    }),
+    {}
+  );
+  res.render('index', {
+    helpers: {
+      num1() { return num1 },
+      num2() { return num2 },
+      result() { return result },
+    },
+    selectedOption: selectedOptionObject,
+  })
+})
+
+
+
+app.listen(port, () => {
+  console.log('Server is running on http://localhost:3000')
+})
